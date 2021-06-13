d2c8f6d24c4d4974881ebb0ef12d28fa6af02478
adshre <aditi.telang208@gmail.com>
2021-06-13 15:32:21 +0530

    Create AmazonStringToInteger.py

---
+++ AmazonStringToInteger.py
@@ -0,0 +1,28 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.strip()
+        if len(s) == 0:  # test case of only white spaces / no char in string
+            return 0
+        sign = 1  # to check positive or negative
+        num = ""
+        if s[0] == '-':
+            sign = -1
+            s = s[1:]  # to remove special character check of is digit, other cahr like '.' included in check
+        elif s[0] == '+':
+            sign = 1
+            s = s[1:]
+        for each in s:
+            if not each.isdigit(): #check if not digit
+                break  # get out of loop
+            num = num + each  # add to answe
+        if len(num) == 0 :  # if nothing is added in ans, all alpha char or special char
+            return 0
+        else :
+            num = int(num)  # convert string to int
+            if sign == -1 :
+                num = -1 * num  # to negative 
+                return max(-2 ** 31, num)  # To return in range if negative 
+            return min(2 ** 31 -1 , num)  # To return in range if positive
+            
+
+        
